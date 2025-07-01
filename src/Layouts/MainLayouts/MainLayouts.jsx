cca8938c91b3b2cdc7d1e5591d85eb9fa98201ea
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-01 07:30:07 +0600

    Layout jsx 🚢

--- src/Layouts/MainLayouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Sheard/Navbar/Navbar";
 import Footer from "../../Sheard/Footer/Footer";
-
+// Layout jsx
 const MainLayouts = () => {
   return (
     <div className="md:w-11/12 md:mx-auto">
